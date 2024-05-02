3f6d3854ace4fb3f51ca22bde463d81a73919978
abgbro <codepath@abhigarlapati.com>
2024-05-02 18:25:10 -0400

    Initial Commit

---
+++ HotProspects/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by user256510 on 5/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by user256510 on 5/2/24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
